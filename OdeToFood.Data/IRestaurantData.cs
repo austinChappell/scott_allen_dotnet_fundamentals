f381d4a6ca72c0ec6c1e99d773e61ea7b0e399e2
Austin <austin.w.chappell@gmail.com>
2019-12-03 06:21:08 -0600

    initial commit

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,40 @@
+using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+  public interface IRestaurantData
+  {
+    IEnumerable<Restaurant> GetRestaurantsByName(string name);
+    Restaurant GetById(int id);
+  }
+
+  public class InMemoryRestaurantData : IRestaurantData
+  {
+    List<Restaurant> restaurants;
+
+    public InMemoryRestaurantData()
+    {
+      restaurants = new List<Restaurant>()
+      {
+        new Restaurant { Id = 1, Name = "Austin's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian },
+        new Restaurant { Id = 2, Name = "Fuzzy's Taco Shop", Location = "Dallas", Cuisine = CuisineType.Mexican }
+      };
+    }
+
+    public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+    {
+      return from r in restaurants
+             where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+             orderby r.Name
+             select r;
+    }
+
+    public Restaurant GetById(int id)
+    {
+      return restaurants.SingleOrDefault(r => r.Id == id);
+    }
+  }
+}
