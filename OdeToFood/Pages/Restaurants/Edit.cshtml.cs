e1f12f98a0febf17a28430ec495752907e36391e
Austin <austin.w.chappell@gmail.com>
2019-12-04 06:30:13 -0600

    can redirect after edit

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -38,9 +38,14 @@ namespace OdeToFood.Pages.Restaurants
 
     public IActionResult OnPost()
     {
+      if (ModelState.IsValid)
+      {
+        Restaurant = restaurantData.Update(Restaurant);
+        restaurantData.Commit();
+        return RedirectToPage("./Details", new { restaurantId = Restaurant.Id });
+      }
+
       Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
-      Restaurant = restaurantData.Update(Restaurant);
-      restaurantData.Commit();
       return Page();
     }
   }
